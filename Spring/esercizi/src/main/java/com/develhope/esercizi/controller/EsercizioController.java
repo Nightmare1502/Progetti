7d2cf077278717840d763c126ee178b8eefbae16
Enrico <enri.bellagamba@gmail.com>
2025-06-05 14:04:42 +0200

    aggiunto esercizio spring GetMapping e RequestParam

---
+++ Spring/esercizi/src/main/java/com/develhope/esercizi/controller/EsercizioController.java
@@ -0,0 +1,19 @@
+package com.develhope.esercizi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class EsercizioController {
+
+@GetMapping("/ciao")
+    public String hello(@RequestParam (required = true) String nome,
+                        @RequestParam (required = true) String regione) {
+    return "Ciao " + nome + ", com'è il tempo in " + regione + "?";
+
+}
+
+}
